34eebb321c5208f567cdc44d494aff60cf2e3993
Andrea Bondi <abondi@paypal.com>
2018-08-28 09:03:50 +0100

    Initial Commit

---
+++ SPBinApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SPBinApp
+//
+//  Created by Bondi, Andrea on 28/08/2018.
+//  Copyright © 2018 Bondi, Andrea. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
